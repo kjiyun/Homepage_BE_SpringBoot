1f0c64e403b06dfc604beb3c53060dcc431bef2b
yumzen <yeom.ji.eun35@gmail.com>
2024-07-12 01:23:14 +0900

    Fix: BaseEntity 수정

--- src/main/java/kahlua/KahluaProject/domain/BaseEntity.java
+++ src/main/java/kahlua/KahluaProject/domain/BaseEntity.java
@@ -1,21 +1,28 @@
 package kahlua.KahluaProject.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
-    @Column(updatable = false)
     @CreatedDate
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP")
     private LocalDateTime created_at;
 
     @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMP")
     private LocalDateTime updated_at;
+
+    @Column(name = "deleted_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime deletedAt;
 }
