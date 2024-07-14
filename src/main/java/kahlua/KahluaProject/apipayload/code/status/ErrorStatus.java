dc35b0d623fa7e33466812c12c2bc81d9cd3a272
yumzen <yeom.ji.eun35@gmail.com>
2024-07-15 02:20:09 +0900

    #16 Feat: 예외처리

--- src/main/java/kahlua/KahluaProject/apipayload/code/status/ErrorStatus.java
+++ src/main/java/kahlua/KahluaProject/apipayload/code/status/ErrorStatus.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import javax.management.loading.MLetContent;
+
 @Getter
 @AllArgsConstructor
 public enum ErrorStatus implements BaseCode {
@@ -15,6 +17,14 @@ public enum ErrorStatus implements BaseCode {
 
     // 로그인 실패
     LOGIN_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "LOGIN FAIL", "아이디 또는 비밀번호를 확인하세요"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER NOT FOUND", "사용자를 찾을 수 없습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "FORBIDDEN", ""),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "권한이 없습니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.NOT_FOUND, "PASSWORD_NOT_MATCH", "비밀번호가 틀렸습니다."),
+    PASSWORD_INVALID(HttpStatus.NOT_FOUND, "PASSWORD_INVALID", "유효하지 않은 password"),
+    TOKEN_INVALID(HttpStatus.NOT_FOUND, "TOKEN_INVALID", "토큰 유효하지 않음"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "s"),
+
 
     // 세션 에러
     SESSION_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "유효하지 않은 세션입니다.");
