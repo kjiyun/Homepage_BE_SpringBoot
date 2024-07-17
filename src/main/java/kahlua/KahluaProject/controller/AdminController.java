909c7c10ff05f4604c4b98542ac2efc14b5ac47b
woogieon8on <swp0927@naver.com>
2024-07-17 21:56:44 +0900

    #17 Feat: add admin ticket list get method

--- src/main/java/kahlua/KahluaProject/controller/AdminController.java
+++ src/main/java/kahlua/KahluaProject/controller/AdminController.java
@@ -3,7 +3,9 @@ package kahlua.KahluaProject.controller;
 import kahlua.KahluaProject.apipayload.ApiResponse;
 import kahlua.KahluaProject.dto.response.ApplyGetResponse;
 import kahlua.KahluaProject.dto.response.ApplyListResponse;
+import kahlua.KahluaProject.dto.response.TicketListResponse;
 import kahlua.KahluaProject.service.ApplyService;
+import kahlua.KahluaProject.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/admin")
 public class AdminController {
 
-
     private final ApplyService applyService;
+    private final TicketService ticketService;
 
     @GetMapping("/apply")
     public ApiResponse<ApplyListResponse> getApplyList() {
@@ -28,5 +30,23 @@ public class AdminController {
     public ApiResponse<ApplyGetResponse> getApplyDetail(@PathVariable Long applyId) {
         ApplyGetResponse applyGetResponse = applyService.getApply(applyId);
         return ApiResponse.onSuccess(applyGetResponse);
+    }
+
+    @GetMapping("/tickets")
+    public ApiResponse<TicketListResponse> getTicketList() {
+        TicketListResponse ticketListResponse = ticketService.getTicketList();
+        return ApiResponse.onSuccess(ticketListResponse);
+    }
+
+    @GetMapping("/tickets/general/")
+    public ApiResponse<TicketListResponse> getGeneralTicketList() {
+        TicketListResponse ticketListResponse = ticketService.getGeneralTicketList();
+        return ApiResponse.onSuccess(ticketListResponse);
+    }
+
+    @GetMapping("/tickets/freshman/")
+    public ApiResponse<TicketListResponse> getFreshmanTicketList() {
+        TicketListResponse ticketListResponse = ticketService.getFreshmanTicketList();
+        return ApiResponse.onSuccess(ticketListResponse);
     }
 }
