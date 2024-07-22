9bab2af9afe0fe8d2a0ba817c9c297e0912366b7
yumzen <yeom.ji.eun35@gmail.com>
2024-07-22 14:40:26 +0900

    #23 Feat: 오류 처리

--- src/main/java/kahlua/KahluaProject/confg/SecurityConfig.java
+++ src/main/java/kahlua/KahluaProject/confg/SecurityConfig.java
@@ -49,8 +49,8 @@ public class SecurityConfig {
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
         );
         http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers( "/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/v1/auth/sign-out/**", "v1/auth/recreate/**","/v1/user/**", "/v1/admin/**", "/error/**").authenticated()
+                        .requestMatchers( "/api-docs/**", "/swagger-ui/**", "/swagger-ui.html/**", "/v3/api-docs/**", "/swagger-ui/index.html#/**").permitAll()
+                        .requestMatchers("/v1/auth/sign-out/**", "v1/auth/recreate/**","/v1/user/**", "/v1/admin/**").authenticated()
                         .anyRequest().permitAll())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(exceptionFilter, JwtFilter.class);
