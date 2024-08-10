dbb430c87b07acda1d69b35317d5760cc199b810
woogieon8on <140885810+woogieon8on@users.noreply.github.com>
2024-08-10 22:17:44 +0900

    #3 Fix: modify swagger url (#54)

--- src/main/java/kahlua/KahluaProject/config/SecurityConfig.java
+++ src/main/java/kahlua/KahluaProject/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
         );
         http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers( "/api-docs/**", "/swagger-ui/**", "/swagger-ui.html/**", "/v3/api-docs/**", "/swagger-ui/index.html#/**", "/api/v3/api-docs/**").permitAll()
+                        .requestMatchers( "/api-docs/**", "/swagger-ui/**", "/swagger-ui.html/**", "/v3/api-docs/**", "/swagger-ui/index.html#/**").permitAll()
                         .requestMatchers("/v1/auth/sign-out/**", "v1/auth/recreate/**","/v1/user/**", "/v1/admin/**").authenticated()
                         .anyRequest().permitAll())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
