d9e22357d81f94b662a12e85e3d851cbe787f78d
woogieon8on <140885810+woogieon8on@users.noreply.github.com>
2024-08-17 20:19:55 +0900

    #3 Fix: fix CORS error (#62)

--- src/main/java/kahlua/KahluaProject/config/CorsMvcConfig.java
+++ src/main/java/kahlua/KahluaProject/config/CorsMvcConfig.java
@@ -11,6 +11,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://kahluaband.com/", "http://kahluaband.com/", "https://kahluabandver20-caminobelllos-projects.vercel.app");
+                .allowedOrigins("http://localhost:3000", "https://kahluaband.com/", "http://kahluaband.com/",
+                        "https://www.kahluaband.com/", "http://www.kahluaband.com/", "https://kahluabandver20-caminobelllos-projects.vercel.app");
     }
 }
