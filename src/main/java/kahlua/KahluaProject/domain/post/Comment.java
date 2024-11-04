64daabcff1a60d414a2feac4aadd0d158d3fa744
Jiyun <kjy5952@naver.com>
2024-11-04 21:32:37 +0900

    #74 Feat: modify comment domain

--- src/main/java/kahlua/KahluaProject/domain/post/Comment.java
+++ src/main/java/kahlua/KahluaProject/domain/post/Comment.java
@@ -2,10 +2,14 @@ package kahlua.KahluaProject.domain.post;
 
 import jakarta.persistence.*;
 import kahlua.KahluaProject.domain.BaseEntity;
+import kahlua.KahluaProject.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Builder
@@ -16,13 +20,29 @@ import lombok.NoArgsConstructor;
 public class Comment extends BaseEntity {
 
     @Id
-    @Column(name="comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
     private Long id;
 
     @Column(nullable = false)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "POST_ID")
+    @JoinColumn(name = "post_id")
     private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 부모 댓글
+    @ManyToOne
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    private boolean isDeleted = false;
+
+    // 자식 댓글
+    @OneToMany(mappedBy = "parentComment")
+    private List<Comment> childComments = new ArrayList<>();
 }
