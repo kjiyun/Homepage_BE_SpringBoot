515b044c3236fc1cc97e53e2c41edaca64965f48
dldusgh318 <pololydotoly@gmail.com>
2025-02-10 22:27:10 +0900

    feat: 공연 리스트 api 비즈니스 로직 구현 (#136)

---
+++ src/main/java/kahlua/KahluaProject/converter/TicketInfoConverter.java
@@ -0,0 +1,23 @@
+package kahlua.KahluaProject.converter;
+
+import kahlua.KahluaProject.domain.ticketInfo.PerformanceStatus;
+import kahlua.KahluaProject.domain.ticketInfo.TicketInfo;
+import kahlua.KahluaProject.dto.ticketInfo.response.PerformanceRes;
+
+import java.time.LocalDateTime;
+
+import static kahlua.KahluaProject.domain.ticketInfo.PerformanceStatus.CLOSED;
+import static kahlua.KahluaProject.domain.ticketInfo.PerformanceStatus.OPEN;
+
+public class TicketInfoConverter {
+
+    public static PerformanceRes.performanceDto toPerformanceDto(TicketInfo info,PerformanceStatus status){
+        return PerformanceRes.performanceDto.builder()
+                .ticketInfoId(info.getId())
+                .title(info.getTicketInfoData().toString())
+                .status(status)
+                .posterUrl(info.getPosterImageUrl())
+                .build();
+    }
+
+}
--- src/main/java/kahlua/KahluaProject/repository/ticket/TicketInfoRepository.java
+++ src/main/java/kahlua/KahluaProject/repository/ticket/TicketInfoRepository.java
@@ -1,7 +1,15 @@
 package kahlua.KahluaProject.repository.ticket;
 
 import kahlua.KahluaProject.domain.ticketInfo.TicketInfo;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TicketInfoRepository extends JpaRepository<TicketInfo, Long> {
+    List<TicketInfo> findAllByOrderByIdDesc(Pageable pageable);
+
+    List<TicketInfo> findAllByIdLessThanOrderByIdDesc(Long cursor, Pageable pageable);
 }
