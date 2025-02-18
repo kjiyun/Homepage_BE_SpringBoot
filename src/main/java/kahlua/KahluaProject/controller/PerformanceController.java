e1480f8e09e0a41bf610675f65a0ca22112025d1
dldusgh318 <pololydotoly@gmail.com>
2025-02-18 20:27:54 +0900

    Merge upstream main

--- src/main/java/kahlua/KahluaProject/controller/PerformanceController.java
+++ src/main/java/kahlua/KahluaProject/controller/PerformanceController.java
@@ -3,10 +3,13 @@ package kahlua.KahluaProject.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kahlua.KahluaProject.apipayload.ApiResponse;
+import kahlua.KahluaProject.dto.ticketInfo.request.TicketInfoRequest;
 import kahlua.KahluaProject.dto.ticketInfo.response.PerformanceRes;
 import kahlua.KahluaProject.dto.ticketInfo.response.TicketInfoResponse;
+import kahlua.KahluaProject.security.AuthDetails;
 import kahlua.KahluaProject.service.PerformanceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "공연 페이지", description = "공연페이지 관련 API")
@@ -28,6 +31,13 @@ public class PerformanceController {
     @Operation(summary = "공연 상세정보 조회 API", description = "특정 공연의 상세정보를 조회하는 API입니다.")
     public ApiResponse<PerformanceRes.performanceInfoDto> getPerformanceInfo(@PathVariable Long ticketInfoId){
         return ApiResponse.onSuccess(performanceService.getPerformanceInfo(ticketInfoId));
+    }
+
+    @PostMapping("/create")
+    @Operation(summary = "공연정보 생성 API", description = "새로운 공연을 생성하는 API입니다.")
+    public ApiResponse<TicketInfoResponse> createPerformance(@RequestBody TicketInfoRequest ticketCreateRequest, @AuthenticationPrincipal AuthDetails authDetails){
+        TicketInfoResponse ticketinfoResponse = performanceService.createPerformance(ticketCreateRequest, authDetails.user());
+        return ApiResponse.onSuccess(ticketinfoResponse);
     }
 }
 
